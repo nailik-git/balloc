378ee07a7c8a986748189677b69546fd17273bab
NailikGit <kilian.smeets@web.de>
2025-02-18 00:36:56 +0100

    update nob

--- nob.c
+++ nob.c
@@ -1,5 +1,7 @@
 #define NOB_IMPLEMENTATION
 #include "nob.h"
+#define BUILD "build/"
+#define SRC "src/"
 
 int main(int argc, char* argv[]) {
   NOB_GO_REBUILD_URSELF(argc, argv);
@@ -8,17 +10,17 @@ int main(int argc, char* argv[]) {
 
   Nob_Cmd cmd = {0};
 
-  if(nob_needs_rebuild("balloc.o", (const char*[]){"src/balloc.c", "src/balloc.h"}, 2)) {
+  if(nob_needs_rebuild(BUILD"balloc.o", (const char*[]){SRC"balloc.c", SRC"balloc.h"}, 2)) {
     nob_cmd_append(&cmd, "clang", "-c", "-g", "-O3", "-Wall", "-Wextra");
-    nob_cmd_append(&cmd, "src/balloc.c");
+    nob_cmd_append(&cmd, SRC"balloc.c", "-o", BUILD"balloc.o");
     if(!nob_cmd_run_sync_and_reset(&cmd)) return 1;
   } else {
     nob_log(NOB_INFO, "'balloc.o' up to date");
   }
 
-  if(nob_needs_rebuild("build/test", (const char*[]){"src/test.c", "balloc.o", "src/balloc.h"}, 3)) {
+  if(nob_needs_rebuild(BUILD"test", (const char*[]){SRC"test.c", SRC"balloc.h", BUILD"balloc.o"}, 3)) {
     nob_cmd_append(&cmd, "clang", "-g", "-O3", "-Wall", "-Wextra");
-    nob_cmd_append(&cmd, "-o", "build/test", "balloc.o", "src/test.c");
+    nob_cmd_append(&cmd, "-o", BUILD"test", "balloc.o", SRC"test.c");
     if(!nob_cmd_run_sync_and_reset(&cmd)) return 1;
   } else {
     nob_log(NOB_INFO, "'test' up to date");
