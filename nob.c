42963dc80e50a19ab7357e5b081a4a5bdd8b1097
NailikGit <kilian.smeets@web.de>
2025-02-17 17:10:26 +0100

    move to src dir & add test in nob

--- nob.c
+++ nob.c
@@ -8,20 +8,26 @@ int main(int argc, char* argv[]) {
 
   Nob_Cmd cmd = {0};
 
-  if(nob_needs_rebuild("balloc.o", (const char*[]){"balloc.c", "balloc.h"}, 2)) {
+  if(nob_needs_rebuild("balloc.o", (const char*[]){"src/balloc.c", "src/balloc.h"}, 2)) {
     nob_cmd_append(&cmd, "clang", "-c", "-g", "-O3", "-Wall", "-Wextra");
-    nob_cmd_append(&cmd, "balloc.c");
+    nob_cmd_append(&cmd, "src/balloc.c");
     if(!nob_cmd_run_sync_and_reset(&cmd)) return 1;
   } else {
     nob_log(NOB_INFO, "'balloc.o' up to date");
   }
 
-  if(nob_needs_rebuild("build/test", (const char*[]){"test.c", "balloc.o", "balloc.h"}, 3)) {
+  if(nob_needs_rebuild("build/test", (const char*[]){"src/test.c", "balloc.o", "src/balloc.h"}, 3)) {
     nob_cmd_append(&cmd, "clang", "-g", "-O3", "-Wall", "-Wextra");
-    nob_cmd_append(&cmd, "-o", "build/test", "balloc.o", "test.c");
-    if(!nob_cmd_run_sync(cmd)) return 1;
+    nob_cmd_append(&cmd, "-o", "build/test", "balloc.o", "src/test.c");
+    if(!nob_cmd_run_sync_and_reset(&cmd)) return 1;
   } else {
     nob_log(NOB_INFO, "'test' up to date");
+  }
+
+  if(argc > 1 && strncmp(argv[1], "test", 4) == 0) {
+    nob_cmd_append(&cmd, "build/test");
+    nob_log(NOB_INFO, "running test");
+    nob_cmd_run_sync(cmd);
   }
 
   return 0;
